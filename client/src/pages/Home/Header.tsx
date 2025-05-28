e407aeb8e2d1f42e0bd21ed921d95ec46a394625
minjeongss <dotoriido@gmail.com>
2025-05-29 02:51:24 +0900

    feat: figma 화면에 맞춘 홈의 헤더 적용

--- client/src/pages/Home/Header.tsx
+++ client/src/pages/Home/Header.tsx
@@ -2,11 +2,11 @@ const Header = () => {
   return (
     <div className="w-4/5 h-[500px] flex flex-row justify-center items-center bg-[url('/src/assets/images/star.png')] bg-contain bg-no-repeat relative">
       <div className="absolute bottom-30 text-center bg-[#FAFAFA]">
-        <p>
-          <span>역사가 흐르는 날이에요,</span>
-          <span>지혜로운 벗님</span>
+        <p className="text-[38px]">
+          <span className=" font-medium">역사가 흐르는 날이에요,</span>
+          <span className="font-semibold">지혜로운 벗님</span>
         </p>
-        <p>오늘도 한국사를 탐구해볼까요?</p>
+        <p className="text-3xl font-medium">오늘도 한국사를 탐구해볼까요?</p>
       </div>
       <img
         className="w-[120px] h-auto absolute bottom-4 right-18"
