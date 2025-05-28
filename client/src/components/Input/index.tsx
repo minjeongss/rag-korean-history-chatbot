53d503dda47560d6d5b06dff2ca76bd5b64ec91a
minjeongss <dotoriido@gmail.com>
2025-05-29 02:45:23 +0900

    feat: 입력창 텍스트 모드/보이스 모드 구별

--- client/src/components/Input/index.tsx
+++ client/src/components/Input/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import Button from "../Button";
 
 const Input = () => {
+  const [mode, setMode] = useState<"text" | "voice">("text");
   return (
-    <div className="w-4/5 h-32 flex flex-col bg-[#FFFFFF] p-4 rounded-[16px] border border-2 border-[#CCCCCC]">
-      <textarea name="question" rows={5} className=" resize-none" />
-      <div className="flex flex-row justify-end gap-4">
-        <Button imgSrc="/src/assets/images/voice.png" />
-        <Button imgSrc="/src/assets/images/send.png" />
+    <div className="w-4/5 h-35 flex flex-col gap-4 bg-[#FFFFFF] p-4 rounded-[16px] border border-2 border-[#CCCCCC]">
+      <textarea
+        name="question"
+        rows={10}
+        className="flex-grow resize-none outline-none"
+      />
+      <div className="flex flex-row justify-end items-end gap-4">
+        {mode === "text" ? (
+          <>
+            <Button imgSrc="/src/assets/images/voice.png" />
+            <Button imgSrc="/src/assets/images/send.png" />
+          </>
+        ) : (
+          <Button imgSrc="/src/assets/images/stop.png" />
+        )}
       </div>
     </div>
   );
