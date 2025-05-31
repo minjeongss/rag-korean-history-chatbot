c1f41ac42fc8a6a9dbbb613ec9976ce4ca6114df
minjeongss <dotoriido@gmail.com>
2025-06-01 03:13:16 +0900

    feat: 메인 화면 문구 반말로 변경

--- client/src/pages/Home/Header.tsx
+++ client/src/pages/Home/Header.tsx
@@ -3,10 +3,10 @@ const Header = () => {
     <div className="w-3/5 h-[500px] flex flex-row justify-center items-center bg-[url('/src/assets/images/star.png')] bg-contain bg-no-repeat relative">
       <div className="absolute bottom-30 text-center bg-[#FAFAFA]">
         <p className="text-3xl">
-          <span className=" font-medium">역사가 흐르는 날이에요,</span>
-          <span className="font-semibold">지혜로운 벗님</span>
+          <span className=" font-medium">역사가 흐르고 있어,</span>
+          <span className="font-semibold">지혜로운 친구야!</span>
         </p>
-        <p className="text-2xl font-medium">오늘도 한국사를 탐구해볼까요?</p>
+        <p className="text-2xl font-medium">오늘도 함께 한국사를 탐구해볼까?</p>
       </div>
       <img
         className="w-[120px] h-auto absolute bottom-4 right-18"
